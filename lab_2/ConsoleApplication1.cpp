0a12ad336de759ad2c4df3c97c34b1e03860ed59
Ivan <vany.kozlov5@mail.ru>
2023-03-15 13:26:07 +0500

    class Point
    creation of the class Point

---
+++ lab_2/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+class Point {
+protected:
+    int _x, _y;
+public:
+    Point() : _x(0), _y(0) { cout << "Point()\n"; }
+    Point(int x, int y): _x(x), _y(y) { cout << "Point(int x, int y)\n"; }
+    Point(Point& pnt) : _x(pnt._x), _y(pnt._y) { cout << "Point(Point& pnt)\n"; }
+    virtual ~Point() { cout << "~Point()\n"; }
+
+    void printPoint() {
+        cout << _x << " " << _y;
+    }
+};
+
+int main()
+{
+    Point first(20, 25);
+    first.printPoint();
+
+    Point* second = new Point(first);
+    second->printPoint();
+    delete second;
+}

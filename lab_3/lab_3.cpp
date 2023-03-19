c0e4cb1985c898eeffe343db51e41568f8c810e0
Ivan <vany.kozlov5@mail.ru>
2023-03-19 15:25:37 +0500

    Desc1 class

--- lab_3/lab_3.cpp
+++ lab_3/lab_3.cpp
@@ -1,61 +1,69 @@
 ﻿#include <iostream>
+#include <typeinfo>
 using namespace std;
 
 
 class Base {
 public:
-    string message;
+    virtual string getInfo() {
+        return "Base object\n";
+    }
 
-    Base() : message("Hello") { cout << "Base()\n"; }
-    Base(int size) { cout << "Base(int size)\n"; }
-    Base(Base& arr) : message(arr.message) { cout << "Base(Base& arr)\n"; }
+    Base() { cout << "Base()\n"; }
 
-    virtual ~Base() {}
+    virtual ~Base() { cout << "~Base()\n"; }
 };
 
 
 class Desc1 : public Base {
+public:
+    string getInfo() override {
+        return "Desc1 object\n";
+    }
 
+    Desc1() { cout << "Desc1()\n"; }
+
+    ~Desc1() { cout << "~Desc1()\n"; }
 };
-
-
 
 
 class _Array {
 private:
-    Base* array;
+    Base** array;
 public:
 
-    int _size;
-    _Array() : _size(0)  { 
-        array = new Base[0];
+    _Array() { 
+        array = new Base*;
         cout << "_Array()\n"; 
     }
-    _Array(int size) : _size(size) { 
-        array = new Base[_size];
+
+    _Array(int size) { 
+        array = new Base* [size];
         cout << "_Array(int size)\n"; 
     }
-    //_Array(_Array& arr) : ptr(arr.ptr) { cout << "_Array(_Array& arr)\n"; } // должны скопировать всё содержимое arr;
 
-    virtual ~_Array() {
-        delete[] array;
-        cout << "~_Array()\n";
+    _Array(_Array& arr){
+        array = arr.array;
+        cout << "_Array(_Array& arr)\n"; 
+    } // должны скопировать всё содержимое arr;
+
+    ~_Array() { cout << "~_Array()\n"; }
+
+    void SetObject(int pozition, Base& base) {
+        array[pozition] = &base;
     }
 
-    void Add(int pozition, Base& base) {
-        array[pozition] = base;
-        cout << array->message << "\n";
+    void ObjectInfo(int pozition) {
+        cout << array[pozition]->getInfo();
     }
 };
 
 int main()
 {
-    Base test;
+    _Array arr(2);
 
+    Desc1 p;
 
-    _Array massive(2);
-
-    massive.Add(0, test);
-    massive.Add(1, test);
-
+    arr.SetObject(0, p);
+    arr.ObjectInfo(0);
 }

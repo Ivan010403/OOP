aef3413222e56c962648b1555b7a1a8218a1c166
Ivan <vany.kozlov5@mail.ru>
2023-03-19 13:57:52 +0500

    class Array

---
+++ lab_3/lab_3.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+
+
+class Base {
+public:
+    string message;
+
+    Base() : message("Hello") { cout << "Base()\n"; }
+    Base(int size) { cout << "Base(int size)\n"; }
+    Base(Base& arr) : message(arr.message) { cout << "Base(Base& arr)\n"; }
+
+    virtual ~Base() {}
+};
+
+
+class Desc1 : public Base {
+
+};
+
+
+
+
+class _Array {
+private:
+    Base* array;
+public:
+
+    int _size;
+    _Array() : _size(0)  { 
+        array = new Base[0];
+        cout << "_Array()\n"; 
+    }
+    _Array(int size) : _size(size) { 
+        array = new Base[_size];
+        cout << "_Array(int size)\n"; 
+    }
+    //_Array(_Array& arr) : ptr(arr.ptr) { cout << "_Array(_Array& arr)\n"; } // должны скопировать всё содержимое arr;
+
+    virtual ~_Array() {
+        delete[] array;
+        cout << "~_Array()\n";
+    }
+
+    void Add(int pozition, Base& base) {
+        array[pozition] = base;
+        cout << array->message << "\n";
+    }
+};
+
+int main()
+{
+    Base test;
+
+
+    _Array massive(2);
+
+    massive.Add(0, test);
+    massive.Add(1, test);
+
+}

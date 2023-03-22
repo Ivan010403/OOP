2be549de70be9a37920853d3f8f4e26be48a0f2d
Ivan <vany.kozlov5@mail.ru>
2023-03-22 13:55:49 +0500

    Update lab_3.cpp

--- lab_3/lab_3.cpp
+++ lab_3/lab_3.cpp
@@ -6,11 +6,11 @@ using namespace std;
 class Base {
 public:
     virtual string getInfo() {
-        return "Base object\n";
+        return "\nBase object\n";
     }
 
     Base() { cout << "Base()\n"; }
-
+    
     virtual ~Base() { cout << "~Base()\n"; }
 };
 
@@ -18,7 +18,7 @@ public:
 class Desc1 : public Base {
 public:
     string getInfo() override {
-        return "Desc1 object\n";
+        return "\nDesc1 object\n";
     }
 
     Desc1() { cout << "Desc1()\n"; }
@@ -29,7 +29,7 @@ public:
 class Desc1_1 : public Desc1 {
 public:
     string getInfo() override {
-        return "Desc1_1 object\n";
+        return "\nDesc1_1 object\n";
     }
 
     Desc1_1() { cout << "Desc1_1()\n"; }
@@ -40,7 +40,7 @@ public:
 class Desc2 : public Base {
 public:
     string getInfo() override {
-        return "Desc2 object\n";
+        return "\nDesc2 object\n";
     }
 
     Desc2() { cout << "Desc2()\n"; }
@@ -65,15 +65,28 @@ public:
         cout << "_Array(int size)\n"; 
     }
 
-    _Array(_Array& arr){
-        array = arr.array; //reference semantics, если вызовется деструктор arr, то array не будет иметь доступ к памяти по этому адресу. Надо сделать глубокое копирование
+    _Array(_Array& arr) : _size(arr._size) {
+        //array = arr.array; //reference semantics, если вызовется деструктор arr, то array не будет иметь доступ к памяти по этому адресу. Надо сделать глубокое копирование
+
+
+
+        //Base** arr_copy = new Base* [_size];
+        //
+        //for (int i = 0; i < _size; i++)
+        //{
+        //    arr_copy[i] = arr.array[0] -> clone();
+        //    cout << typeid(arr_copy[i]).name() << "\n";
+        //}
+
+        //array = arr_copy;
+
         cout << "_Array(_Array& arr)\n"; 
     } 
 
     ~_Array() { 
         for (int i = 0; i < _size; i++)
         {
-            if (array[i]) delete[] array[i];
+            if (array[i]) delete array[i];
         }
         delete[] array;
         cout << "~_Array()\n"; 
@@ -84,13 +97,34 @@ public:
     }
 
     void ObjectInfo(int pozition) {
-        cout << array[pozition]->getInfo();
+        cout << array[pozition]->getInfo() <<"\n";
     }
 
     void RemoveObj(int pozition) {
         delete array[pozition];
         array[pozition] = nullptr;
     }
+
+    int AddObject(Base* base) {
+        for (int i = 0; i < _size; i++)
+        {
+            if (array[i]==nullptr) {
+                SetObject(i, base);
+                return 0;
+            }
+        }
+
+        _size += 1;
+        Base** arr_copy = new Base * [_size];
+        for (int i = 0; i < _size - 1; i++)
+        {
+            arr_copy[i] = array[i];
+        }
+        arr_copy[_size-1] = base;
+
+        array = arr_copy;
+    }
+
 };
 
 int main()
@@ -107,10 +141,15 @@ int main()
         if (random_number == 2) arr.SetObject(i, new Desc2);
     }
 
-    for (int i = 0; i < size; i++)
+    arr.AddObject(new Desc1);
+    arr.ObjectInfo(3);
+    arr.AddObject(new Desc1);
+    arr.ObjectInfo(4);
+ /*   for (int i = 0; i < size; i++)
     {
-        random_number = rand() % 3;
+        random_number = rand() % 2;
         if (random_number == 0) arr.ObjectInfo(i);
         if (random_number == 1) arr.RemoveObj(i);
-    }
+    }*/
+
 }

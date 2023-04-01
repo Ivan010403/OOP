83b6c9f3cf7162b2fda385fa2ecbcc61aba6f247
Ivan <vany.kozlov5@mail.ru>
2023-04-01 12:09:13 +0500

    Create lab_5.cpp

---
+++ lab_5/lab_5.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+class Car {
+private:
+    string classname = "Car";
+
+public:
+    void redefinedMethod() {
+        cout << "Method of Car class\n";
+    }
+};
+
+class passengerCar : public Car {
+public:
+    void redefinedMethod() {
+        cout << "Method of passengerCar class\n";
+    }
+};
+
+class Niva4x4 : public passengerCar{
+
+};
+
+class Truck : public Car {
+
+};
+
+
+int main()
+{
+    
+    //-----------------------
+    passengerCar obj;
+    obj.redefinedMethod();
+    obj.Car::redefinedMethod();
+    //----------------------
+}

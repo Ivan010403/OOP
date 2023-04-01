d047669feaf033b2ad070f68b08ec589f23a1f89
Ivan <vany.kozlov5@mail.ru>
2023-04-01 13:44:10 +0500

    virtual method

--- lab_5/lab_5.cpp
+++ lab_5/lab_5.cpp
@@ -2,24 +2,41 @@
 using namespace std;
 
 class Car {
-private:
+public:
     string classname = "Car";
 
-public:
+
     void redefinedMethod() {
         cout << "Method of Car class\n";
     }
+
+    virtual void overridenMethod() {
+        cout << "Method of Car class\n";
+    }
+
+    Car() { cout << "Car()\n"; }
+    virtual ~Car() { cout << "~Car()\n"; }
 };
 
 class passengerCar : public Car {
 public:
+    string classname = "passengerCar";
+
     void redefinedMethod() {
         cout << "Method of passengerCar class\n";
     }
+
+    void overridenMethod() override {
+        cout << "Method of passengerCar class\n";
+    }
+
+    passengerCar() { cout << "passengerCar()\n"; }
+    ~passengerCar() override { cout << "~passengerCar()\n"; }
 };
 
 class Niva4x4 : public passengerCar{
-
+public:
+    string classname = "Niva4x4";
 };
 
 class Truck : public Car {
@@ -34,5 +51,9 @@ int main()
     passengerCar obj;
     obj.redefinedMethod();
     obj.Car::redefinedMethod();
+    //----------------------
+    Car* test = new passengerCar();
+    test->overridenMethod();
+    delete test;
     //----------------------
 }
